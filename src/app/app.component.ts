d6c173732ace3fb9989b484a0918edb5ab1aedf6
Marjan Bos <mb@senanga.net>
2018-07-11 17:16:06 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gpe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'gpe';
+}
